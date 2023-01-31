ce15534b768ccd937d647f3e3547a3035a175092
Noob-hacker369 <anushitsatapathy121@gmail.com>
2023-01-31 19:43:30 +0530

    first commit

---
+++ kali.sh
@@ -0,0 +1,15 @@
+# This is a default distribution 
+# plug-in. Do not modify this file as 
+# your changes will be overwritten on 
+# next update. If you want customize 
+# installation, please make a copy.
+DISTRO_NAME="Kali"
+
+TARBALL_STRIP_OPT=0
+
+TARBALL_URL["aarch64"]="https://github.com/RandomCoderOrg/fs-cook/releases/download/kali-01/kali-rolling-raw-arm64.tar.gz"
+TARBALL_SHA256["aarch64"]="bcd311ae53fc6576881e5f8cbbc007fc7c49a5348894e03fa97cc2397ef24fff" 
+distro_setup() {
+	# Don't update gvfs-daemons and udisks2
+	run_proot_cmd apt-mark hold gvfs-daemons udisks2
+}
